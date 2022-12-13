5fbc0bf7eb69307e488aca9ab25f800489b6f48a
adacai1982 <adacai1982@hotmail.com>
2022-12-13 17:01:17 +0800

    fix spider

--- BlockchainSpider/spiders/labels/labelcloudexchange.py
+++ BlockchainSpider/spiders/labels/labelcloudexchange.py
@@ -12,8 +12,8 @@ from BlockchainSpider.items import LabelAddressItem, LabelTransactionItem, Label
 
 class LabelsCloudSpiderExchange(scrapy.Spider):
     exchangeList = ['Bitfinex', 'Bitstamp' , 'Bittrex', 'Binance', 'Coinbase', 'Coinone', 'CoinMetro', 'Coinhako', 
-    'Coinsquare', 'Crypto.com', 'Deribit', 'Gate.io', 'Gemini', 'Hotbit', 'Huobi', 'Korbit', 'Kraken', 'KuCoin', 
-    'OKX', 'Poloniex', 'Paribu', 'Tornado.Cash', 'Upbit','Exchange', 'MaskEX']
+    'Coinsquare', 'Crypto-com', 'Deribit', 'Gate-io', 'Gemini', 'Hotbit', 'Huobi', 'Korbit', 'Kraken', 'KuCoin', 
+    'OKX', 'Poloniex', 'Paribu', 'Tornado-Cash', 'Upbit','Exchange', 'MaskEX']
     name = 'labels.labelcloud.exchange'
     custom_settings = {
         'DOWNLOADER_MIDDLEWARES': {
